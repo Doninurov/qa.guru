136f1ff37994edd709dcdfad8380efcf1b433391
doninurov <donistark94@gmail.com>
2024-03-15 23:54:25 +0500

    add firsttest 1 ref

--- src/test/java/firstTest.java
+++ src/test/java/firstTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -12,18 +13,43 @@ public class firstTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
     }
+    //https://demoqa.com/automation-practice-form
 
     @Test
     void fillFormTest() {
         open("/text-box");
-
 
         $("#userName").setValue("Alex Egorov");
         $("#userEmail").setValue("alex@egorov.com");
         $("#currentAddress").setValue("Some address 1");
         $("#permanentAddress").setValue("Another address 1");
         $("#submit").click();
-        $("#output").shouldHave(text("Alex Egorov"), text("alex@egorov.com"),
-                text("Some address 1"), text("Another address 1"));
+
+        $("#output").shouldHave(text("Alex Egorov"), text("alex@egorov.com"), text("Some address 1"), text("Another address 1"));
+    }
+
+    @Test
+    void fillFormTest1() {
+        open("/automation-practice-form");
+
+        $("#firstName").setValue("Alex");
+        $("#lastName").setValue("Egorov");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("9778103332");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("June");
+        $(".react-datepicker__year-select").selectOption("1994");
+        $(".react-datepicker__day--001").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("112333.png");
+        $("#currentAddress").setValue("Some address 1");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Noida")).click();
+        $("#submit").click();
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
     }
 }
