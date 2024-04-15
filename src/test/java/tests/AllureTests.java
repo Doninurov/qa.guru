a7fd7087ee14e84d04bbd996e5fabf8f99108b8f
doninurov <donistark94@gmail.com>
2024-04-15 15:51:49 +0500

    add dad

--- src/test/java/tests/AllureTests.java
+++ src/test/java/tests/AllureTests.java
@@ -1,8 +1,23 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.*;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import tests.data.WebSteps;
+
+import java.io.ByteArrayInputStream;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.attachment;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
 
 public class AllureTests {
     @Test
@@ -15,7 +30,7 @@ public class AllureTests {
     public void testStaticLabels() {
     }
 
-    @Test
+
     public void testDynamicLabels() {
         Allure.getLifecycle().updateTestCase(
                 t -> t.setName("Создание Issue для авторизованного пользователя")
@@ -26,4 +41,83 @@ public class AllureTests {
         Allure.label("severity", SeverityLevel.CRITICAL.value());
         Allure.link("Testing", "https://testing.github.com");
     }
+    @Owner("Doninurov")  // просто тест с подключенным Алюром но без отображения названия шагов
+    @DisplayName("Создание Issue для авторизованного пользователя")
+    @Test
+    public void testIssueSearch() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $("#issues-tab").click();
+        $(withText("#80")).should(Condition.exist);
+    }
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE = 80;
+
+    @DisplayName("Тест с лямда функций")
+    @Test
+    public void testLambdaStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com");
+        });
+        step("Ищем репозиторий " + REPOSITORY, () -> {
+            $(".header-search-button").click();
+            $("#query-builder-test").sendKeys(REPOSITORY);
+            $("#query-builder-test").submit();
+        });
+        step("Кликаем по ссылке репозитория " + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+        step("Открываем таб Issues", () -> {
+            $("#issues-tab").click();
+        });
+        step("Проверяем наличие Issue с номером " + ISSUE, () -> {
+            $(withText("#" + ISSUE)).should(Condition.exist);
+        });
+    }
+    @Test
+    public void testAnnotatedStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.openIssuesTab();
+        steps.shouldSeeIssueWithNumber(ISSUE);
+
+    }
+    @Test
+    public void testLambdaAttachments() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com");
+            attachment("Source", webdriver().driver().source());
+            byte[] screenshot = ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+            attachScreenshot("Screenshot", screenshot); });
+    }
+    private void attachScreenshot(String name, byte[] screenshot) {
+        Allure.addAttachment(name, new ByteArrayInputStream(screenshot));
+    }
+
+    @Test
+    public void testAnnotatedAttachments() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.takeScreenshot();
+    }
+
+
+
 }
